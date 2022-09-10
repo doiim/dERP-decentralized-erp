3d207bf72028734247ca93e0d09161e93f4ac3b9
Mateus <94379499+mattdalpi@users.noreply.github.com>
2022-09-10 14:46:33 +0000

    Add base component structure to pages

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -3,62 +3,26 @@
 </template>
 
 <script>
-  // import { inject } from 'vue'
+  import { inject } from 'vue'
   // import { ethers } from 'ethers'
 
-  // // import CustomButton from './CustomButton.vue'
+  import { useAccountStore } from '../stores/account'
   import AccountWidget from './AccountWidget.vue'
 
-  // import { useAccountStore } from '../stores/account'
-  // import localhostAddresses from '../../deploys/localhost.json'
-  // import counterArtifact from '../../artifacts/contracts/counter.sol/Counter.json'
-
   export default {
-  //   setup() {
-  //     const store = useAccountStore()
-  //     const getWalletSigner = inject('getWalletSigner')
-  //     return { store, getWalletSigner }
-  //   },
-  //   data() {
-  //     return {
-  //       counter: 0,
-  //       waiting: false,
-  //     }
-  //   },
+    setup() {
+      const store = useAccountStore()
+      const getWalletSigner = inject('getWalletSigner')
+      return { store, getWalletSigner }
+    },
+    data() {
+      return {
+        temp: false,
+      }
+    },
     components: { AccountWidget },
-  //   watch: {
-  //     // Whenever account changes, this function will run
-  //     async 'store.account'(account) {
-  //       if (account) {
-  //         this.refreshCounter()
-  //       }
-  //     },
-  //   },
-  //   methods: {
-  //     async refreshCounter() {
-  //       // The Contract object
-  //       const counterContract = new ethers.Contract(
-  //         localhostAddresses.counter,
-  //         counterArtifact.abi,
-  //         this.getWalletSigner(),
-  //       )
-  //       this.counter = await counterContract.getCount()
-  //       // console.log(await this.store.provider.send('eth_chainId', []))
-  //       return
-  //     },
-  //     async incrementCounter() {
-  //       const counterContract = new ethers.Contract(
-  //         localhostAddresses.counter,
-  //         counterArtifact.abi,
-  //         this.getWalletSigner(),
-  //       )
-  //       const transaction = await counterContract.incrementCounter()
-  //       this.waiting = true
-  //       await transaction.wait()
-  //       await this.refreshCounter()
-  //       this.waiting = false
-  //     },
-  //   },
+    watch: {},
+    methods: {},
   }
 </script>
 

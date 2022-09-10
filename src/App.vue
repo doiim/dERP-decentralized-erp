0e831a479f51090b7880427fd7fdd09ef6e2a63c
Mateus <94379499+mattdalpi@users.noreply.github.com>
2022-09-10 13:55:38 +0000

    Create a simple sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Header Image" src="./assets/header.png" class="my-4"/>
-  <HelloWorld />
-  <img alt="Header Image" src="./assets/footer.png" class="my-4"/>
+  <div class="flex">
+    <div class="flex flex-col w-64 h-screen px-4 py-8 overflow-y-auto border-r">
+      <img
+        src="./assets/derp_logo.png"
+        class="mt-1"
+        style="width: 120px"
+        alt=""
+      />
+      <h1 class="normal-case">dERP</h1>
+
+      <div class="flex flex-col justify-between mt-6">
+        <aside>
+          <ul>
+            <li class="my-1">
+              <router-link to="/">Home</router-link>
+            </li>
+            <li class="my-1">
+              <router-link to="/itens">Itens</router-link>
+            </li>
+            <li class="my-1">
+              <router-link to="/recipes">Recipes</router-link>
+            </li>
+            <li class="my-1">
+              <router-link to="/inventory">Inventory</router-link>
+            </li>
+            <li class="my-1">
+              <router-link to="/invoice">Invoice</router-link>
+            </li>
+          </ul>
+        </aside>
+      </div>
+    </div>
+    <div class="w-full p-4 m-8 overflow-y-aut bg-white">
+      <div class="flex p-40">
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
 </template>

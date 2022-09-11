06c25aedaded0618e159c23ab2027c698005dbd9
Mateus <94379499+mattdalpi@users.noreply.github.com>
2022-09-11 12:33:41 +0000

    Fix find Mumbai config files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,27 +1,28 @@
-require("@nomiclabs/hardhat-waffle");
-const fs = require('fs');
+require('@nomiclabs/hardhat-waffle')
+const fs = require('fs')
 
 let apiMumbai, seedMumbai
-
-try{
-  apiMumbai = fs.readFileSync(".api.mumbai").toString().trim();
-  seedMumbai = fs.readFileSync(".seed.mumbai").toString().trim();
-} catch (err) {
-  console.log('Error fetching seeds')
-}
-
-module.exports = {
-  solidity: "0.8.9",
+let temp = {
+  solidity: '0.8.9',
   networks: {
     hardhat: {
       blockGasLimit: 30000000,
     },
-    mumbai: {
-      url: apiMumbai,
-      accounts: {
-        mnemonic: seedMumbai,
-        count: 1
-      }
-    }
+  },
+}
+
+try {
+  apiMumbai = fs.readFileSync('.api.mumbai').toString().trim()
+  seedMumbai = fs.readFileSync('.seed.mumbai').toString().trim()
+  temp.mumbai = {
+    url: apiMumbai,
+    accounts: {
+      mnemonic: seedMumbai,
+      count: 1,
+    },
   }
-};
+} catch (err) {
+  console.log('Error fetching seeds')
+}
+
+module.exports = temp
